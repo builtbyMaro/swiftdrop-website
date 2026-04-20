3eca99bc3469eedde5b4715f9a6e12efc6d5d56c
Princewill Ubido <princewillubidoo@gmail.com>
2026-04-20 18:48:06 +0100

    image updates for better performance and ui change

--- App.js
+++ App.js
@@ -1,38 +1,42 @@
 // Mobile navbar display
-const mobileNavBtn = document.getElementById('mobile-navbtn')
-const mobileNavBtnClose = document.getElementById('mobile-navbtn-close')
-const mobileMenu = document.querySelector('.mobile-navbar-content')
+const mobileNavBtn = document.getElementById("mobile-navbtn");
+const mobileNavBtnClose = document.getElementById("mobile-navbtn-close");
+const mobileMenu = document.querySelector(".mobile-navbar-content");
 
-mobileNavBtn.addEventListener('click', e => {
-  mobileMenu.classList.toggle("mobile-navbar-show")
+mobileNavBtn.addEventListener("click", (e) => {
+  mobileMenu.classList.toggle("mobile-navbar-show");
 
-  const isOpen = mobileMenu.classList.contains("mobile-navbar-show")
+  const isOpen = mobileMenu.classList.contains("mobile-navbar-show");
 
-  mobileNavBtn.innerHTML = isOpen ? `<i class='bx  bx-x'  ></i>` : `<i class='bx  bx-menu-wider'  ></i>`
+  mobileNavBtn.innerHTML = isOpen
+    ? `<i class='bx  bx-x'  ></i>`
+    : `<i class='bx  bx-menu-wider'  ></i>`;
 });
 
-const navBtn = document.querySelectorAll("#navBtn")
+const navBtn = document.querySelectorAll("#navBtn");
 
-navBtn.forEach(btn => {
-  btn.addEventListener('click', e => {
+navBtn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
     if (mobileMenu.classList.contains("mobile-navbar-show")) {
-      mobileMenu.classList.remove("mobile-navbar-show")
-      mobileNavBtn.innerHTML = "<i class='bx  bx-menu-wider'  ></i>"
+      mobileMenu.classList.remove("mobile-navbar-show");
+      mobileNavBtn.innerHTML = "<i class='bx  bx-menu-wider'  ></i>";
     }
-  })
-})
+  });
+});
 
 // Accordion Content design & display
 const accordionCards = document.querySelectorAll(".accordion-card");
 
-accordionCards.forEach(card => {
+accordionCards.forEach((card) => {
   const content = card.querySelector(".accordion-content");
+  const icon = card.querySelector("#icon");
 
   card.addEventListener("click", () => {
-    accordionCards.forEach(c => {
+    accordionCards.forEach((c) => {
       if (c !== card) {
         c.classList.remove("accordion-active");
         c.querySelector(".accordion-content").style.maxHeight = "0px";
+        icon.className = "bx bx-plus";
       }
     });
 
@@ -40,39 +44,38 @@ accordionCards.forEach(card => {
 
     if (card.classList.contains("accordion-active")) {
       content.style.maxHeight = content.scrollHeight + "px";
+      icon.className = "bx bx-minus";
     } else {
       content.style.maxHeight = "0px";
+      icon.className = "bx bx-plus";
     }
   });
 });
 
-
 // Form validation and submission
-let errorMsg = document.querySelector('#error-message')
-const waitlistForm = document.querySelector('#waitlist-form')
+let errorMsg = document.querySelector("#error-message");
+const waitlistForm = document.querySelector("#waitlist-form");
 
-waitlistForm.addEventListener('submit', e => {
-    e.preventDefault()
-    errorMsg.style.display = 'none'
+waitlistForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  errorMsg.style.display = "none";
 
-    const email = document.querySelector('#email').value.trim()
+  const email = document.querySelector("#email").value.trim();
 
-    if (email === "") {
-        errorMsg.style.display = 'block'
-        return;
-    }
+  if (email === "") {
+    errorMsg.style.display = "block";
+    return;
+  }
 
-    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
-        errorMsg.style.display = 'block'
-        errorMsg.textContent = '*Email not correct*'
-        return;
-    }
-
-    else {
-      errorMsg.style.display = 'block'
-      errorMsg.style.color = 'green'
-      errorMsg.textContent = "*You'll be redirected shortly*"
-      waitlistForm.submit()
-      waitlistForm.reset()
-    }
+  if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+    errorMsg.style.display = "block";
+    errorMsg.textContent = "*Email not correct*";
+    return;
+  } else {
+    errorMsg.style.display = "block";
+    errorMsg.style.color = "green";
+    errorMsg.textContent = "*You'll be redirected shortly*";
+    waitlistForm.submit();
+    waitlistForm.reset();
+  }
 });
